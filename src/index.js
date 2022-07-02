ed6d865fbc68cc3ec2bd62151dcdf620ae7b7dda
masnzzz <z3heng0326@gmail.com>
2022-07-02 22:02:29 +0900

    初期化

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return <h1>Hello React</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
