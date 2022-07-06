c22c79e0d96c4723804eaa45694ca79103a756c1
masnzzz <z3heng0326@gmail.com>
2022-07-06 19:11:16 +0900

    propsでスタイルを変更する

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
+import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
     const onClickButton = () => alert();
-    const contentStyle = {
-        color: 'blue',
-        fontSize: '18px'
-    };
     return (
         <>
-            <h1 style={{ color: 'red' }}>Hello React!</h1>
-            <p style={contentStyle}>this is practice</p>
-            <button onClick={onClickButton}>ボタン</button>
+          <h1 style={{ color: 'red' }}>Hello React!</h1>
+          <ColorfulMessage color='blue' >blue!!!</ColorfulMessage>
+          <ColorfulMessage color='pink' >pink!!!</ColorfulMessage>
+          <button onClick={onClickButton}>ボタン</button>
         </>
     );
 };
