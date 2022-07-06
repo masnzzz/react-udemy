c64414dd5c5060739b86d00cc34819ce9bdfb175
masnzzz <z3heng0326@gmail.com>
2022-07-06 19:18:51 +0900

    useStateを使う
    CountUpで値を更新するボタンを実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
-    const onClickButton = () => alert();
+    const onClickCountUp = () => {
+        setNum(num + 1);
+    };
+    // [動的に変わる引数(Stateを変更する), 静的に更新する関数(更新する)]
+    const [num, setNum] = useState(0);
+
     return (
         <>
-          <h1 style={{ color: 'red' }}>Hello React!</h1>
-          <ColorfulMessage color='blue' >blue!!!</ColorfulMessage>
-          <ColorfulMessage color='pink' >pink!!!</ColorfulMessage>
-          <button onClick={onClickButton}>ボタン</button>
+            <h1 style={{ color: 'red' }}>Hello React!</h1>
+            <ColorfulMessage color='blue' >blue!!!</ColorfulMessage>
+            <ColorfulMessage color='pink' >pink!!!</ColorfulMessage>
+            <button onClick={onClickCountUp}>Count Up!</button>
+            <p>{num}</p>
         </>
     );
 };
