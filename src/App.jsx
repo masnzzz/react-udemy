4d9434a9e1fe6f22e27dc41f4cc53440f9e2d817
masnzzz <z3heng0326@gmail.com>
2022-07-18 14:18:56 +0900

    カウントアップ、オンオフで顔を出す機能を実装
    3の倍数で顔を出現させる

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import React, { useState } from "react";
 import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
+    console.log('最初');
+    // [動的に変わる引数(Stateを変更する), 静的に更新する関数(更新する)]
+    const [num, setNum] = useState(0);
+    // 表示非表示を扱うステート
+    const [faceShowFlag, setFaceShowFlag] = useState(false);
+
     const onClickCountUp = () => {
         setNum(num + 1);
     };
-    // [動的に変わる引数(Stateを変更する), 静的に更新する関数(更新する)]
-    const [num, setNum] = useState(0);
+
+    const onClickSwitchShowFlag = () => {
+        setFaceShowFlag(!faceShowFlag);
+    };
+
+    if (num > 0) {
+        // 3の倍数だけ顔が出る
+        if (num % 3 === 0) {
+            faceShowFlag || setFaceShowFlag(true);
+        } else {
+            faceShowFlag && setFaceShowFlag(false);
+        }
+    }
 
     return (
         <>
@@ -14,7 +31,10 @@ const App = () => {
             <ColorfulMessage color='blue' >blue!!!</ColorfulMessage>
             <ColorfulMessage color='pink' >pink!!!</ColorfulMessage>
             <button onClick={onClickCountUp}>Count Up!</button>
+            <br />
+            <button onClick={onClickSwitchShowFlag}>on/off</button>
             <p>{num}</p>
+            {faceShowFlag && <p>(^^;)</p>}
         </>
     );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  const { color, children } = props;
-  const contentStyle = {
-    // color: color,
-    // propsの名前が一致している場合は省略した書き方もできる
-    color,
-    fontSize: '18px'
-  };
+    const { color, children } = props;
+    const contentStyle = {
+        // color: color,
+        // propsの名前が一致している場合は省略した書き方もできる
+        color,
+        fontSize: '18px'
+    };
 
-  return <p style={contentStyle}>{children}</p>;
+    return <p style={contentStyle}>{children}</p>;
 }
 
 export default ColorfulMessage;
